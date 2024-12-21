a686f7c027208332248e4883aa4d99e1032563d5
raoabdulrafay05 <153934084+raoabdulrafay05@users.noreply.github.com>
2024-12-21 14:25:51 +0500

    Update base.cpp
    Implementing Linked list with the structure node which include medicine data.

--- base.cpp
+++ base.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+struct Medicine
+{
+    int id;
+    string name;
+    int quantity;
+    double price;
+    string expiryDate;
 
+    Medicine(int id, string name, int quantity, double price, string expiryDate)
+        : id(id), name(name), quantity(quantity), price(price), expiryDate(expiryDate) {}
+};
+
+struct Node
+{
+    Medicine *medicine;
+    Node *next;
+
+    Node(Medicine *medicine) : medicine(medicine), next(nullptr) {}
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void addMedicine(Medicine *medicine)
+    {
+        Node *newNode = new Node(medicine);
+        if (!head)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp->next)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
